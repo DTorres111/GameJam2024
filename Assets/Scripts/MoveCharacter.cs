b0322826cce0436ccdb01b721dd8cd94a01661c6
DTorres111 <danilotf353@gmail.com>
2024-07-22 17:12:43 -0400

    Player and shadow movement

--- Assets/Scripts/MoveCharacter.cs
+++ Assets/Scripts/MoveCharacter.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class MoveCharacter : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float speed = 5f;
     public float jumpForce = 10f;
-    private Rigidbody2D rb;
     private bool isGrounded;
+    private bool jumpRequest;
+    private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.sharedMaterial = Resources.Load<PhysicsMaterial2D>("NoFriction");
     }
 
     void Update()
     {
-        // Move left and right
-        float moveX = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        // Movement
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        // Jump
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        // Jumping
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jumpRequest = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (jumpRequest)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            jumpRequest = false;
         }
     }
 
@@ -33,6 +44,7 @@ public class MoveCharacter : MonoBehaviour
         {
             isGrounded = true;
         }
+        
     }
 
     void OnCollisionExit2D(Collision2D collision)
@@ -41,5 +53,21 @@ public class MoveCharacter : MonoBehaviour
         {
             isGrounded = false;
         }
+    }
+
+    
+    void OnCollisionStay2D(Collision2D collision)
+    {
+
+        //Making sure the isGrounded flag is properly set when touching both Ground and Obstacle
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
     }
 }
