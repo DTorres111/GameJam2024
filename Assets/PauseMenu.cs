5ce9bd4b75153e515997cfa08b9c36b754d21b4b
DTorres111 <danilotf353@gmail.com>
2024-07-31 08:27:13 -0400

    Last touches for submission

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -44,6 +45,7 @@ public class PauseMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit();
+        ResumeGame();
+        SceneManager.LoadSceneAsync(0);
     }
 }
